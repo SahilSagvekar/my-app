8146d79f6072dbf3fedcc3743a4e12f897457602
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2025-11-27 16:56:31 +0530

    changes

--- src/lib/cron-monthly.js
+++ src/lib/cron-monthly.js
@@ -4,7 +4,7 @@ async function runCron() {
   try {
     console.log("[CRON] Running monthly cron job...");
 
-    const res = await fetch(process.env.BASE_URL + "/api/cron/monthly");
+    const res = await fetch(process.env.BASE_URLL + "/api/cron/monthly");
     const data = await res.json();
 
     console.log("[CRON RESULT]:", data);
