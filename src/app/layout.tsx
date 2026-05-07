73c7fc6c6dbfaf22a2f5b1f7731945eebd500220
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2026-05-07 18:19:30 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TTGCNQQW"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMX9JSXC"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
@@ -56,7 +56,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TTGCNQQW');`}
+})(window,document,'script','dataLayer','GTM-NMX9JSXC');`}
         </Script>
 
         {/* Google Analytics (gtag.js) */}
