698a321d9054ca9ac82f6f35cc339f7b49ae292b
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2026-05-07 18:57:13 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,26 +5,22 @@ import "./globals.css";
 import { AuthProvider } from "./../components/auth/AuthContext";
 import { Toaster } from 'sonner';
 import { buildMetadata } from "@/lib/seo";
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: 'swap',
 });
-
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-jetbrains-mono",
   display: 'swap',
 });
-
 export const metadata: Metadata = buildMetadata({
   title: "E8 Productions",
   description:
     "Video production, social media content and digital marketing that helps brands grow.",
   image: "/image.png",
 });
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,42 +31,26 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} ${inter.className} antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - place immediately after <body> */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NMX9JSXC"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W82XVTPF"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-
         <AuthProvider>
           {children}
           <Toaster position="bottom-right" />
         </AuthProvider>
-
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - loads GTM which manages GA4 and all other tags */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NMX9JSXC');`}
-        </Script>
-
-        {/* Google Analytics (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-5ZF3VHTSD0"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-5ZF3VHTSD0');
-          `}
+})(window,document,'script','dataLayer','GTM-W82XVTPF');`}
         </Script>
       </body>
     </html>
